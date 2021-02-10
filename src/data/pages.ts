b4349b3620d42e8aeb0e04d476bb595f11253e17
shiuhong <shiuhong.cheong@shopee.com>
2021-02-10 23:34:38 +0800

    update

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -1,7 +1,7 @@
 export const pages = [
   {text: 'Home', href: '/'},
-  {text: 'About Ron', href: '/about'},
-  {text: 'Projects', href: '/projects'},
-  {text: 'Experiences', href: '/experiences'},
-  {text: 'Education', href: '/education'},
+  {text: 'About Ron', href: '/about', icon: '/media/images/home_about_icon.png'},
+  {text: 'Projects', href: '/projects', icon: '/media/images/home_projects_icon.png'},
+  {text: 'Experiences', href: '/experiences', icon: '/media/images/home_experiences_icon.png'},
+  {text: 'Education', href: '/education', icon: '/media/images/home_education_icon.png'},
 ]
